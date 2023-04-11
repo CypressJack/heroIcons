b42820924fd4a3ae7abd5809c204e89809c5c41b
CypressJack <wadecroft@gmail.com>
2023-04-11 21:26:59 +0100

    update to fix folder copy error

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 #! /usr/bin/env node
 
-const fs = require('fs').promises;
+const fs = require('fs-extra');
 const path = require('path');
 
 async function copyFiles() {
@@ -8,14 +8,13 @@ async function copyFiles() {
     // Copy HeroIcon.jsx to ./src/components/Icons/
     const heroIconSrcPath = path.join(__dirname, 'HeroIcon.jsx');
     const heroIconDestPath = path.join(process.cwd(), 'src', 'components', 'Icons', 'HeroIcon.jsx');
-    await fs.copyFile(heroIconSrcPath, heroIconDestPath);
+    await fs.copy(heroIconSrcPath, heroIconDestPath);
     console.log(`Copied HeroIcon.jsx to ${heroIconDestPath}`);
 
     // Copy heroIcons directory to ./public/
     const heroIconsSrcPath = path.join(__dirname, 'heroIcons');
     const heroIconsDestPath = path.join(process.cwd(), 'public', 'heroIcons');
-    await fs.mkdir(heroIconsDestPath, { recursive: true });
-    await fs.copyFile(heroIconsSrcPath, heroIconsDestPath);
+    await fs.copy(heroIconsSrcPath, heroIconsDestPath);
     console.log(`Copied heroIcons to ${heroIconsDestPath}`);
   } catch (error) {
     console.error(error);
