2e4ea2c55430e8fbd235cbdcf837a1732e2bd127
CypressJack <wadecroft@gmail.com>
2023-04-11 21:23:52 +0100

    update working directory

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ async function copyFiles() {
   try {
     // Copy HeroIcon.jsx to ./src/components/Icons/
     const heroIconSrcPath = path.join(__dirname, 'HeroIcon.jsx');
-    const heroIconDestPath = path.join(__dirname, 'src', 'components', 'Icons', 'HeroIcon.jsx');
+    const heroIconDestPath = path.join(process.cwd(), 'src', 'components', 'Icons', 'HeroIcon.jsx');
     await fs.copyFile(heroIconSrcPath, heroIconDestPath);
     console.log(`Copied HeroIcon.jsx to ${heroIconDestPath}`);
 
     // Copy heroIcons directory to ./public/
     const heroIconsSrcPath = path.join(__dirname, 'heroIcons');
-    const heroIconsDestPath = path.join(__dirname, 'public', 'heroIcons');
+    const heroIconsDestPath = path.join(process.cwd(), 'public', 'heroIcons');
     await fs.mkdir(heroIconsDestPath, { recursive: true });
     await fs.copyFile(heroIconsSrcPath, heroIconsDestPath);
     console.log(`Copied heroIcons to ${heroIconsDestPath}`);
